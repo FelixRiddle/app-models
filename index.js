5d9f4c47fdeabac54a332e4986ac596b62ac8cd0
Felix <fedecraft3312@gmail.com>
2024-02-26 18:16:40 -0300

    Added models as default exports

--- index.js
+++ index.js
@@ -4,9 +4,23 @@ const MSQLDC_FetchENV = require("./src/connection/MSQLDC_FetchENV");
 // Models
 const MODEL = require("./src/models/SQL/index");
 
+const Category = require("./src/models/SQL/Category");
+const DebugPropertyImageUpload = require("./src/models/SQL/DebugPropertyImageUpload");
+const Price = require("./src/models/SQL/Price");
+const Property = require("./src/models/SQL/Price");
+const User = require("./src/models/SQL/User");
+const UserMessages = require("./src/models/SQL/UserMessages");
 
 module.exports = {
     MSQLDC_FetchENV,
     
     MODEL,
+    
+    // Default exports
+    Category,
+    DebugPropertyImageUpload,
+    Price,
+    Property,
+    User,
+    UserMessages,
 }
