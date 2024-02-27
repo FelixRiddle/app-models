50fc14de18242147e63578553f43d0c369bf03ee
Felix <fedecraft3312@gmail.com>
2024-02-27 10:31:32 -0300

    Mixed property with price

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const MODEL = require("./src/models/SQL/index");
 const Category = require("./src/models/SQL/Category");
 const DebugPropertyImageUpload = require("./src/models/SQL/DebugPropertyImageUpload");
 const Price = require("./src/models/SQL/Price");
-const Property = require("./src/models/SQL/Price");
+const Property = require("./src/models/SQL/Property");
 const User = require("./src/models/SQL/User");
 const UserMessages = require("./src/models/SQL/UserMessages");
 
